e84e6bf7555c3a8a212c420638f656662f5ed457
Abhishek Das <abhishek.das82@gmail.com>
2020-08-05 18:32:47 +0530

    Update settings.php

--- linkedin_learning/settings.php
+++ linkedin_learning/settings.php
@@ -32,6 +32,8 @@ if ($ADMIN->fulltree) {
     $settings->add(new admin_setting_configtext('linkedin_client_id', new lang_string('linkedin_client_id', 'block_linkedin_learning'),'','',PARAM_ALPHANUMEXT));
     $settings->add(new admin_setting_configtext('linkedin_client_secret', new lang_string('linkedin_client_secret', 'block_linkedin_learning'),'','',PARAM_ALPHANUMEXT));
 	
+	$settings->add(new admin_setting_configtext('linkedin_client_carousel', new lang_string('linkedin_client_carousel_title', 'block_linkedin_learning'), new lang_string('linkedin_client_carousel_description', 'block_linkedin_learning'), 4, PARAM_INT));
+	
 	$options = array(1 => 'Once in a Day', 2 => 'Once in a week', 3 => 'Once in a month');
     $settings->add(new admin_setting_configselect('linkedin_cron_run', new lang_string('linkedin_cron_title', 'block_linkedin_learning'),new lang_string('linkedin_cron_description', 'block_linkedin_learning'), 0, $options));
 }
