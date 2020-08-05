3912b6996f4e934dfc2a4c28d56fbfeb66050832
Abhishek Das <abhishek.das82@gmail.com>
2020-08-05 18:31:49 +0530

    Update block_linkedin_learning.php

--- linkedin_learning/block_linkedin_learning.php
+++ linkedin_learning/block_linkedin_learning.php
@@ -22,12 +22,23 @@
  */
 
 class block_linkedin_learning extends block_base {
+	
+	protected const HOUSER  = 'General Office';
 
     /**
      * Initialise the block.
      */
     public function init() {
         $this->title = get_string('pluginname', 'block_linkedin_learning');
+    }
+	
+	/**
+     * Gets the contents of the block (course view)
+     *
+     * @return object An object with the contents
+     */
+    public function isguestuser($user = null) {
+        return false;
     }
 
     /**
@@ -44,17 +55,25 @@ class block_linkedin_learning extends block_base {
      * @return stdClass the content
      */
     public function get_content() {
-        global $CFG, $OUTPUT;
+        global $CFG, $OUTPUT, $USER, $DB;
 		
 		if ($this->content !== null) {
             return $this->content;
         }
-		$courses = $this->linkedin_courses();
-		$output = $OUTPUT->render_from_template('block_linkedin_learning/linkedin_courses',$courses);
-		
 		$this->content = new StdClass;
-
-        $this->content->text = $output;
+		
+		//Checking for HO users
+		$condition = array('userid' => $USER->id);
+		$houser = $DB->get_record('custom_fields', $condition, 'store_type_description');
+		if($houser->store_type_description == self::HOUSER || is_siteadmin()) {
+			
+			$courses = $this->linkedin_courses();
+			$content .= $OUTPUT->render_from_template('block_linkedin_learning/linkedin_courses',$courses);
+			$this->content->text = $content;
+			
+		} else {
+			$this->content->text = '';
+		}
         
         return $this->content;
     }
@@ -65,13 +84,15 @@ class block_linkedin_learning extends block_base {
      * @output: array $params 
      */
 	 public function linkedin_courses() : array {
-		 global $DB;
+		 global $DB, $CFG;
 		 $params = array();
+		 $limit = $CFG->linkedin_client_carousel > 0?$CFG->linkedin_client_carousel:4;
+
 		 $sql = "SELECT id, urn, title, completiontime, 
 						TIME_FORMAT(SEC_TO_TIME(completiontime),'%kh %im') AS timeToCompelete, 
-						weblaunchurl, author, image, courselevel
+						weblaunchurl, ssolaunchurl, author, image, courselevel
 							FROM {linkedin_learning_courses}";
-		 $courses = $DB->get_records_sql($sql);
+		 $courses = $DB->get_records_sql($sql, null, 0, $limit);
 		 if (count($courses) > 0) {
 			 $i = 0;
 			 foreach($courses AS $key => $val) {
@@ -88,8 +109,10 @@ class block_linkedin_learning extends block_base {
 				$params['course'][$i]['author'] = $val->author;
 				$params['course'][$i]['image'] = $val->image;
 				$params['course'][$i]['courselevel'] = $val->courselevel; 
+				$params['course'][$i]['ssolaunchurl'] = $val->ssolaunchurl; 
 				$i++;
 			 }
+			 $params['total_slider_course'] = $limit;
 		 }
 		 return $params;
 	 }
